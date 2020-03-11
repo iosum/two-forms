4dc6f8ba40fb7cd57eb95e3be4e363c7cdba1955
Chia-Ying Ho <42313352+iosum@users.noreply.github.com>
2020-03-11 09:26:39 -0400

    add startform code

---
+++ TwoForms/TwoForms/StartForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TwoForms
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nextButton = new System.Windows.Forms.Button();
+            this.clickMeButton = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextButton.Location = new System.Drawing.Point(609, 348);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(121, 56);
+            this.nextButton.TabIndex = 0;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // clickMeButton
+            // 
+            this.clickMeButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickMeButton.Location = new System.Drawing.Point(277, 198);
+            this.clickMeButton.Name = "clickMeButton";
+            this.clickMeButton.Size = new System.Drawing.Size(209, 88);
+            this.clickMeButton.TabIndex = 1;
+            this.clickMeButton.Text = "Click Me";
+            this.clickMeButton.UseVisualStyleBackColor = true;
+            this.clickMeButton.Click += new System.EventHandler(this.clickMeButton_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabel.Location = new System.Drawing.Point(334, 91);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(0, 33);
+            this.messageLabel.TabIndex = 2;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.clickMeButton);
+            this.Controls.Add(this.nextButton);
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Start Form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StartForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button clickMeButton;
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
+
